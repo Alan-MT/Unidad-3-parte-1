6af328d1d0512655192bfde06ee73c302f238dad
Alan-MT <moralesalan235@gmail.com>
2021-06-09 14:01:45 -0600

    ejercicio 6 java

---
+++ Ejercicio6_20.java
@@ -0,0 +1,15 @@
+public class Ejercicio6_20 {
+
+	public static void main(String args[]) {
+		double aumento;
+		int num;
+		num = (int)(Math.random()*200);
+		System.out.println("El numero aleatorio es "+num);
+		aumento = (num*30)/100;
+		double nu = num+aumento;
+		System.out.println("El numero aumento es "+nu);
+	}
+
+
+}
+
