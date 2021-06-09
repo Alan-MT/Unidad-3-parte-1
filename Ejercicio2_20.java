8611a38fd11a377de1a26993f32c5018d8073025
Alan-MT <moralesalan235@gmail.com>
2021-06-09 13:57:56 -0600

    Ejercicio2 java

---
+++ Ejercicio2_20.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Ejercicio2_20{
+
+	public static void main(String[] args){
+		Scanner scanner = new Scanner(System.in);
+		System. out.println("Ingrese la capital");
+		String capital;
+		System. out.println("Ingrese el pais");
+		String pais;
+		pais = scanner.nextLine();
+		capital = scanner.nextLine();
+		System.out.println(capital+" Es la capital de "+pais);
+	}
+
+
+}
+
