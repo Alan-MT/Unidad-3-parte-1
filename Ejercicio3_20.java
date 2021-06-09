8ba876e2e9eff1313eeebbbf4b3f85058ceece65
Alan-MT <moralesalan235@gmail.com>
2021-06-09 13:59:04 -0600

    Ejercicio 3 java

---
+++ Ejercicio3_20.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Ejercicio3_20 {
+
+	public static void main(String args[]){
+		Scanner scanner = new Scanner(System.in);
+		int doble;
+		int num;
+		int triple;
+		num = scanner.nextInt();
+		doble = 2*num;
+		triple = 3*num;
+		System.out.println("El doble del numero ingresado es "+doble);
+		System.out.println("El triple del numero ingresado es "+triple);
+	}
+
+
+}
+
