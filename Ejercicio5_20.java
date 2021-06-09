9e1cdb4c3969f4754fbf8806534cf561a1813065
Alan-MT <moralesalan235@gmail.com>
2021-06-09 14:00:42 -0600

    Ejercico 5 java

---
+++ Ejercicio5_20.java
@@ -0,0 +1,23 @@
+/* Este codigo ha sido generado por el modulo psexport 20180802-w32 de PSeInt.
+Es posible que el codigo generado no sea completamente correcto. Si encuentra
+errores por favor reportelos en el foro (http://pseint.sourceforge.net). */
+
+// En java, el nombre de un archivo fuente debe coincidir con el nombre de la clase que contiene,
+// por lo que este archivo debería llamarse "EJERCICIO5.java."
+import java.util.*;
+
+public class Ejercicio5_20 {
+
+	public static void main(String args[]){
+		Scanner scanner = new Scanner(System.in);
+		int num;
+		int siguiente;
+		System.out.println("Ingrese el numero");
+		num = scanner.nextInt();
+		siguiente = num+1;
+		System.out.println("El numero siguiente es "+siguiente);
+	}
+
+
+}
+
