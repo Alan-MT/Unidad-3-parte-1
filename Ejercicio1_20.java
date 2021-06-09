74cef32ee93d38bc6923428d66af0a86c5010e95
Alan-MT <moralesalan235@gmail.com>
2021-06-09 10:25:53 -0600

    error2

--- Ejercicio1_20.java
+++ Ejercicio1_20.java
@@ -1,13 +1,12 @@
-import java.util.*;
+import java.util.Scanner;
 public class Ejercicio1_20 {
-	public static void main(String args[]){
+	public static void main(String[] args){
 		Scanner scanner = new Scanner(System.in);
 		int numero1;
 		int numero2;
-		double resultado;
 		numero1 = scanner.nextInt();
 		numero2 = scanner.nextInt();
-		resultado = numero1+numero2;
+		int resultado = numero1+numero2;
 		System.out.println("El resultado de la suma es: "+resultado);
 	}
 
