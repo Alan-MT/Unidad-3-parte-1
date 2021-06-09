cbc792ade21786954b05083e55c5f03c9eb07d32
Alan-MT <moralesalan235@gmail.com>
2021-06-08 21:21:43 -0600

    Ejercicio 1. java

---
+++ Ejercicio1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Ejercicio1 {
+
+	public static void main(String args[]){
+		Scanner scanner = new Scanner(System.in);
+		int numero1;
+		int numero2;
+		double resultado;
+		numero1 = scanner.nextInt();
+		numero2 = scanner.nextInt();
+		resultado = numero1+numero2;
+		System.out.println("El resultado de la suma es: "+resultado);
+	}
+
+
+}
+
